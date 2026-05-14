f0bf85f8acaa01e5c29a2e395320ae47f4aa775f
Andrés Guzmán <andyguzper@gmail.com>
2026-05-14 11:37:40 -0600

    Send raw data diretly to the serial port for processing.

--- ecg_main/ecg_main.ino
+++ ecg_main/ecg_main.ino
@@ -1,21 +1,9 @@
-#include "filters.h"
-#include "pan_tompkins.h"
 #include "ble_server.h"
 #include <ArduinoJson.h>
 
-// Parametros de procesamiento
+// Parametros de muestreo
 static const int PIN_ADC = 34;
-static const int FS = 500;              // Frecuencia de muestreo (500 Hz)
-static const int NOTCH_FREQ = 60;       // Frecuencia del filtro notch (60 Hz)
-static const float NOTCH_Q = 30.0f;     // Factor de calidad del filtro notch
-static const float HP_FREQ = 0.5f;      // Frecuencia de corte del filtro pasa-alto (0.5 Hz)
-static const float LP_FREQ = 40.0f;     // Frecuencia de corte del filtro pasa-bajo
-
-// Instancias
-Biquad notchFilt, hpFilt, lpFilt;  // Instancias de filtros 
-PanTompkins pt;             
-
-float dcEstimate = 2048;   // ADC 12-bit centroide 
+static const int FS = 500;              // Frecuencia de muestreo (500 Hz) 
 
 // Timer
 hw_timer_t* sampleTimer = nullptr;
@@ -28,11 +16,6 @@ void setup(){
     analogReadResolution(12);
     pinMode(PIN_ADC, INPUT);
 
-    // Inicializar filtros
-    notchFilt = makeNotch(FS, NOTCH_FREQ, NOTCH_Q);
-    hpFilt = makeHP(FS, HP_FREQ);
-    lpFilt = makeLP(FS, LP_FREQ);
-
     // BLE
     bleBegin("ECG-ESP32");
 
@@ -41,7 +24,7 @@ void setup(){
     timerAttachInterrupt(sampleTimer, &onTimer);
     timerAlarm(sampleTimer, 500, true, 0);    
 
-    Serial.println("[OK] Pipeline listo.");
+    Serial.println("[OK] Raw data acquisition ready.");
 }
 
 // Loop
@@ -49,39 +32,21 @@ void loop(){
     if(!sampleReady) return;
     sampleReady = false;
 
-    // 1. Leer ADC
-    float raw = analogRead(PIN_ADC);
+    // 1. Leer ADC (12-bit)
+    int raw = analogRead(PIN_ADC);
 
-    // 2. Remover DC 
-    dcEstimate = 0.999f * dcEstimate + 0.001f * raw;
-    float x = raw - dcEstimate;
-
-    // 3. Filtros
-    float xN = notchFilt.process(x);
-    float xH = hpFilt.process(xN);
-    float xL = lpFilt.process(xH);
-
-    // 4. Pan-Tompkins
-    float bpm = pt.update(xL);
-    bool rPeak = (bpm > 0);
-
-    // 5. Enviar por BLE cada SEND_EVERY muestras
+    // 2. Enviar raw data por BLE cada SEND_EVERY muestras
     static int sampleCount = 0;
     static const int SEND_EVERY = 5;
 
     sampleCount++;
-        if (sampleCount % SEND_EVERY == 0) {
-        StaticJsonDocument<128> doc;
+    if (sampleCount % SEND_EVERY == 0) {
+        StaticJsonDocument<64> doc;
         doc["t"] = millis();
-        doc["raw"] = (int)raw;
-        doc["ecg"] = xL;
-        if(rPeak){
-            doc["bpm"] = bpm;
-            doc["rPeak"] = true;
-        }
+        doc["raw"] = raw;
 
-        char buf[128];
+        char buf[64];
         serializeJson(doc, buf);
-        bleSend(buf);  
-        }
+        bleSend(buf);
+    }
 }
